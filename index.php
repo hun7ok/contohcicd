2bbad03c58054eb9a5418da9d198d35f26fc1587
erwandevtalahost <erwansmartdata123@gmail.com>
2021-08-24 23:07:06 +0800

    testst
    Signed-off-by: erwandevtalahost <erwansmartdata123@gmail.com>

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
  echo "adakah dirinya, bisa terima ku seadanya, ";
  echo "<b>INI ADALAH TESTING SAJA YA</b>"
  echo "<b>INI ADALAH PERUBAHAN KE 2</b>"
+ echo phpinfo();
 
  //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
  //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
